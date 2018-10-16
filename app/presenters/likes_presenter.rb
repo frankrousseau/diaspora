c30ef82c0b6ecbd8fa70561c115181d8e1ef2c04
Hank Grabowski <hankgrabowski@gmail.com>
2018-10-16 10:52:34 -0400

    Added LikesPresenter class and spec

---
+++ app/presenters/likes_presenter.rb
@@ -0,0 +1,8 @@
+class LikesPresenter < BasePresenter
+  def as_api_json
+    {
+      guid:   @presentable.guid,
+      author: PersonPresenter.new(@presentable.author).as_api_json
+    }
+  end
+end
