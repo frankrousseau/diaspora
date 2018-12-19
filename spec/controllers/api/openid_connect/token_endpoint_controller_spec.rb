c5863abaa8816691c352632a3def20dd70da0c9f
Hank Grabowski <hankgrabowski@gmail.com>
2018-12-18 22:31:20 -0500

    OpenID Connect controller tests use new token factories

--- spec/controllers/api/openid_connect/token_endpoint_controller_spec.rb
+++ spec/controllers/api/openid_connect/token_endpoint_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Api::OpenidConnect::TokenEndpointController, type: :controller, suppress_csrf_verification: :none do
-  let(:auth) { FactoryGirl.create(:auth_with_read) }
+  let(:auth) { FactoryGirl.create(:auth_with_profile_only) }
 
   describe "#create" do
     it "returns 200 on success" do
